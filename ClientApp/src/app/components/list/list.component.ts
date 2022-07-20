2c8bcb921ce6f4ac6b1fd903f65ac30e7a2ef42d
Alex Luibeznov <dr3amyh3ad@gmail.com>
2022-07-20 23:46:32 +0300

    fix bugs

--- ClientApp/src/app/components/list/list.component.ts
+++ ClientApp/src/app/components/list/list.component.ts
@@ -11,7 +11,7 @@ export class ListComponent implements OnInit {
 
   @Output() selectedOption: EventEmitter<number> = new EventEmitter<number>();
 
-  public select: any = null;
+  public select: (null | number) = null;
   public meters: Meters[] = [];
 
   constructor(private http: HttpClient) {}
